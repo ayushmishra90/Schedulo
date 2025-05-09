ae5d91bedcaaef602f29e799f2c57c3a958d7885
ayushmishra90 <145120302+ayushmishra90@users.noreply.github.com>
2025-05-09 21:44:37 +0530

    Delete service_booking directory

--- service_booking/service_app/apps.py
+++
@@ -1,6 +0,0 @@
-# service_app/apps.py
-from django.apps import AppConfig
-
-class ServiceAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'service_app'  # Ensure this is correct
